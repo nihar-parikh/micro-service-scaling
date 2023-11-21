05e5ce4d6771c2b49e7b159bb8594554f9a0835c
nihar-parikh <niharparikh91@gmail.com>
2023-11-21 16:15:50 +0530

    made_changes_to_customer_route

--- src/api/customer.js
+++ src/api/customer.js
@@ -66,6 +66,6 @@ module.exports = (app, channel) => {
 
   app.get("/whoami", (req, res, next) => {
     const processId = process.pid;
-    return res.status(200).json({ msg: "/customer : I am Customer Service", processId });
+    return res.status(200).json({ msg: "/customer : HARE KRISHNA", processId });
   });  
 };
